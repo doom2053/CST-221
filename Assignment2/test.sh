da71e06f9a940436b6af1867b8740b36c45bb63c
doom2053 <31608217+doom2053@users.noreply.github.com>
2018-04-05 17:56:40 -0700

    Add files via upload

---
+++ Assignment2/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "This is a shell script"
+ls -lah
+echo "I am done running ls"
+date
+echo "I am done displaying date"
+id
+echo "I am done displaying user"
+locate -i *test*
+echo "I am done locating test"
